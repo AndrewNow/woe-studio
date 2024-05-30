f68e6a82b569865a30a05d9ca0a403c37927b5ef
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2024-05-30 11:19:27 -0400

    adding netlify adapter

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro'
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     sanity({
       projectId: "n8fc5ydg",
