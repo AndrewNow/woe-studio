3d827e2d775495b1d305ed147cf3fb549e061836
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2025-09-26 14:47:05 -0400

    add stills to nav

--- web/.netlify/functions-internal/ssr/chunks/404_D6PPnlVg.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/404_DiO4OqmZ.mjs').then(n => n._);
-
-export { page };
--- web/.netlify/functions-internal/ssr/chunks/_.._BuwkWBM1.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/__BYuoq-zb.mjs').then(n => n.e);
-
-export { page };
